28f683a2f9220b9322fff542d5a67d2af3d3ea0e
james-mccaffrey <136614316+james-mccaffrey@users.noreply.github.com>
2023-07-19 09:47:10 -0400

    Update main.cpp
    added a user interface in the console
    added functions to calculate distance in miles between 2 longitude latitude coordinates

--- main.cpp
+++ main.cpp
@@ -3,6 +3,25 @@
 #include "fstream"
 #include "sstream"
 #include <cmath>
+#include "iostream"
+
+double toRadians(double degrees) {
+    return degrees * M_PI / 180.0;
+}
+
+double calcDistance(double lat1, double lon1, double lat2, double lon2) {
+    //approximation of the radius of earth in miles
+    double earthRadius = 3960.0;
+
+    double dLat = toRadians(lat2 - lat1);
+    double dLon = toRadians(lon2 - lon1);
+    double a = sin(dLat / 2) * sin(dLat / 2) +
+               cos(toRadians(lat1)) * cos(toRadians(lat2)) *
+               sin(dLon / 2) * sin(dLon / 2);
+    double c = 2 * atan2(sqrt(a), sqrt(1 - a));
+    return earthRadius * c;
+}
+
 int partition(std::vector<std::vector<double>> * sortVector, int low, int high){
     double pivot = (*sortVector)[high][1];
 
@@ -68,7 +87,7 @@ std::vector <std::vector<double>> readVector(std::string fname) {
         stream >> element;
         stationLat = std::stod(element);
 
-        lineVector.push_back(std::sqrt(std::pow(stationLong - longitude, 2) + std::pow(stationLat - latitude, 2)));
+        lineVector.push_back(calcDistance(latitude, longitude, stationLat, stationLong));
 
         depth++;
         inputVec.push_back(lineVector);
@@ -80,10 +99,67 @@ std::vector <std::vector<double>> readVector(std::string fname) {
     return inputVec;
 }
 
+bool exit(){
+    std::string input;
+    std::cout << "\ninput 'yes' to exit: ";
+    std::cin >> input;
+    if (input == "yes") {return true;}
+    else{return false;}
+}
+
+void options(seg_tree ss){
+    bool loop = true;
+    while (loop){
+        std::cout
+        << "please input your choice:\n"
+        << "1 - change a gas price\n"
+        << "2 - get average gas price from staring position\n"
+        << "3 - get average gas price from specified range\n";
+        std::string choice;
+        std::cin >> choice;
+        if (choice == "1"){
+            ss.printVector();
+            std::string stationChoice;
+            std::string newPrice;
+
+            std::cout << "please input the index of which gas price you would like to update:";
+            std::cin >> stationChoice;
+            std::cout << "please input the new gas price";
+            std::cin >> newPrice;
+            try{
+                ss.insert(std::stoi(stationChoice), std::stod(newPrice));
+            }
+            catch(std::exception &err) {
+                std::cout << "invalid input";
+            }
+        }
+        else if (choice == "2"){
+            std::string high;
+            std::cout << "input the maximum distance: ";
+            std::cin >> high;
+            std::cout << "\nthe average gas price in this area is: $" << ss.getAvg(std::stod(high)) << "\n";
+        }
+        else if (choice == "3"){
+            std::string low;
+            std::string high;
+            std::cout << "input the minumum distance: ";
+            std::cin >> low;
+            std::cout << "\ninput the maximum distance: ";
+            std::cin >> high;
+            std::cout << "\nthe average gas price in this area is: $" << ss.getAvg(std::stod(low),std::stod(high)) << "\n";
+        }
+        else {
+            std::cout << "invalid input\n";
+        }
+        loop = !exit();
+    }
+}
+
 int main(int argc, char*argv[]) {
     std::vector<std::vector<double>> intervalVec = readVector(argv[1]);
     seg_tree* ss = new seg_tree(intervalVec);
-
+    options(*ss);
     ss->writeFile(argv[1]);
+    std::cout << "dot file has been written\n";
     return 0;
 }
