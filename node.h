a20096620a61cd21641d8bfcf419f360bedb9e3c
james-mccaffrey <136614316+james-mccaffrey@users.noreply.github.com>
2023-07-20 10:24:25 -0400

    Update node.h
    added comments

--- node.h
+++ node.h
@@ -1,3 +1,10 @@
+//Group 3
+//James McCaffrey
+//Shuichi Kameda
+//Evan Ung
+//Michael Gilkeson
+//Segment Trees
+//7/24/23
 #ifndef SEGTREE_NODE_H
 #define SEGTREE_NODE_H
 
@@ -13,7 +20,6 @@ private:
     Node* right;
 public:
     Node();
-    // Overloading
     Node(double allPrice);
     Node(Node* left, Node* right);
     ~Node();
