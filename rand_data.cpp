382eaaa10d73948ea54698479d934d00bde9d492
ShuichiK1 <78115987+ShuichiK1@users.noreply.github.com>
2023-07-23 11:54:08 -0400

    Added rand_data.cpp
    Supplementary program that lets you quickly create a dataset containing an arbitrary amount of lines.

---
+++ rand_data.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <iomanip>
+
+// referenced from https://www.geeksforgeeks.org/generate-a-random-float-number-in-cpp/
+double random_double(double min, double max) {
+    double random = ((double) rand()) / (double) RAND_MAX;
+    double diff = max - min;
+    double result = random * diff;
+    return min + result;
+}
+
+int main() {
+    // random seed
+    srand(time(0));
+
+    double initial_lat = 0.0;
+    double initial_lon = 0.0;
+    int max_num = 0;
+
+    std::cout
+            << "Input starting latitude, longitude, and number\n"
+            << "of lines in dataset in the following order:\n"
+            << "<latitude> <longitude> <data>\n";
+
+    std::cin >> initial_lat >> initial_lon >> max_num;
+
+    std::string outputName = "data" + std::to_string(max_num) + ".txt";
+    std::ofstream file;
+    file.open(outputName);
+
+    file << std::fixed << std::setprecision(14) << initial_lat << " " << initial_lon << "\n";
+
+    // generates max_num numbers of lines
+    for (int i = 0; i < max_num; ++i) {
+        file << std::fixed << std::setprecision(2) << random_double(2.5, 5.0) << " "
+             << std::fixed << std::setprecision(14) << random_double(-90, 90) << " "
+             << std::fixed << std::setprecision(14) << random_double(-180, 180) << "\n";
+    }
+
+    file.close();
+
+    return 0;
+}
